f004d846836289d7c46187c66a27a4641d1bbc02
Juan Camilo Guarín Peñaranda <jcguarinpenaranda@gmail.com>
2023-04-01 12:22:10 -0500

    Using core

--- src/lib/private-feature-submodule.ts
+++ src/lib/private-feature-submodule.ts
@@ -1,3 +1,9 @@
+import { core } from '@juan/core';
+
 export function privateFeatureSubmodule(): string {
   return 'private-feature-submodule';
+}
+
+export function testCore() {
+  return core();
 }
